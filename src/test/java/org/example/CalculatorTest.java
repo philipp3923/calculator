636f46b00150419679fd3a0f5f037dd25e6a1a68
Philipp Eichwald <philipp3923@icloud.com>
2024-04-21 16:44:07 +0200

    make test public

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class CalculatorTest {
+public class CalculatorTest {
     @Test
     public void testPerformOperation() {
         Calculator calculator = new Calculator();
